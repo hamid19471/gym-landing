371404debd262c68c2f01ec92ab31730770ec8f6
hamid19471 <hamid.19471@gmail.com>
2022-12-22 12:03:29 +0330

    config twin macro for project

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,25 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    optimizeDeps: {
+        esbuildOptions: {
+            target: "es2020",
+        },
+    },
+    esbuild: {
+        // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+        logOverride: { "this-is-undefined-in-esm": "silent" },
+    },
+    plugins: [
+        react({
+            babel: {
+                plugins: [
+                    "babel-plugin-macros",
+                    "babel-plugin-styled-components",
+                ],
+            },
+        }),
+    ],
     resolve: {
         alias: [
             {

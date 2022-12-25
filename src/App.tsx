4d179f3b36c5b8fb8ac18656eddc15669855cdbc
hamid19471 <hamid.19471@gmail.com>
2022-12-25 23:28:38 +0330

    add props to navbar component in app.tsx and create useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Navbar from "@/Components/Navbar/Navbar";
+import { useState } from "react";
 
 function App() {
+    const [selectedPage, setSelectedPage] = useState<string>("home");
     return (
         <div className="bg-gray-20 app">
-            <Navbar />
+            <Navbar
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+            />
         </div>
     );
 }

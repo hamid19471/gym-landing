8deb364aa5846257b37d8a2ba765f2f2f32d75dc
hamid19471 <hamid.19471@gmail.com>
2022-12-25 16:01:36 +0330

    Create menu items data for dynamical

---
+++ src/Data/MenuData/MenuData.ts
@@ -0,0 +1,8 @@
+const MenuData = [
+    { id: 1, name: "Home" },
+    { id: 2, name: "About us" },
+    { id: 3, name: "Benefit" },
+    { id: 4, name: "Contact us" },
+];
+
+export default MenuData;

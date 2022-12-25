d2a5b05cae1dc97aec4422bd2b729df525b6ddc6
hamid19471 <hamid.19471@gmail.com>
2022-12-25 16:02:22 +0330

    Add main bg and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "@/Components/Navbar/Navbar";
+
 function App() {
-    return <div className="app">Application</div>;
+    return (
+        <div className="bg-gray-20 app">
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;

e5e9a28779145de5e4f3070bb38a3e8469f80e93
hamid19471 <hamid.19471@gmail.com>
2022-12-26 12:07:33 +0330

    add fixed header style for change scrollY change background

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import Navbar from "@/Components/Navbar/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "./types/Shared";
 
 function App() {
     const [selectedPage, setSelectedPage] = useState<SelectedPage>(
         SelectedPage.Home,
     );
+    const [isTopPage, setIsTopPage] = useState<boolean>(true);
+
+    useEffect(() => {
+        const topPageHandler = () => {
+            if (window.scrollY == 0) {
+                setIsTopPage(true);
+                setSelectedPage(SelectedPage.Home);
+            }
+            if (window.scrollY != 0) setIsTopPage(false);
+        };
+        window.addEventListener("scroll", topPageHandler);
+        return () => window.removeEventListener("scroll", topPageHandler);
+    }, []);
     return (
         <div className="bg-gray-20 app">
             <Navbar
+                isTopPage={isTopPage}
                 selectedPage={selectedPage}
                 setSelectedPage={setSelectedPage}
             />
